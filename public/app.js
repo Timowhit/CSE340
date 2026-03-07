c76e4ef55f3c92b80677da6b4452bb4de9b29127
Timowhit <whiteheadt4@gmail.com>
2026-03-07 12:47:12 -0700

    Add initial server setup with Express and EJS view engine

--- public/app.js
+++ public/app.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const path = require('path');
 const indexRouter = require('./routes/index');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
 app.use('/', indexRouter);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
